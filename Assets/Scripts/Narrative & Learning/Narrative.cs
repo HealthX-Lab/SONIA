6084f394f3e3db4a3ba6d826d39bdc0bcea09ddf
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2022-05-09 17:29:53 -0400

    Started work on narrative node classes

---
+++ Assets/Scripts/Narrative & Learning/Narrative.cs
@@ -0,0 +1,23 @@
+﻿public class Narrative
+{
+    string Name { get; set; }
+    NarrativeNode StartNode { get; set; }
+
+    public Narrative()
+    {
+        Name = "";
+        StartNode = null;
+    }
+    
+    public Narrative(string name)
+    {
+        Name = name;
+        StartNode = null;
+    }
+    
+    public Narrative(string name, NarrativeNode startNode)
+    {
+        Name = name;
+        StartNode = startNode;
+    }
+}
---
+++ Assets/Scripts/Narrative & Learning/NarrativeNode.cs
@@ -0,0 +1,27 @@
+﻿public class NarrativeNode
+{
+    string Description { set; get; }
+    bool IsEnd { set; get; }
+    NarrativeNode[] Next { set; get; }
+
+    public NarrativeNode()
+    {
+        Description = "";
+        IsEnd = false;
+        Next = null;
+    }
+    
+    public NarrativeNode(string description)
+    {
+        Description = description;
+        IsEnd = false;
+        Next = null;
+    }
+    
+    public NarrativeNode(string description, bool isEnd, NarrativeNode[] next)
+    {
+        Description = description;
+        IsEnd = isEnd;
+        Next = next;
+    }
+}
